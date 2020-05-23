32a4cca918f4d5b0509dc384a5d4fb11c5fbf470
Negar <Negarmsd@users.noreply.github.com>
2020-05-23 23:21:08 +0200

    the is a problem when both are bigger or lower

--- shuffleCards.java
+++ shuffleCards.java
@@ -12,18 +12,326 @@ class shuffleCards{
 		String[] player= disturbiutCards (shuffledCards);
 		String[] showCards = showPlayerCards(player);
 		int[] hiddenCards = hiddenCards();
-		printCards(showCards , hiddenCards);
+		String[] inTable =  returInTable(shuffledCards);
+		String[][] cardsInOrder = ordersOfCards();
+		String[] playerII = returnPlayerII(shuffledCards);
+		String[] playerIIPlayCards = playerIIPlayCards(playerII);
+		int a = cardsInOrder.length;
 
-		playCard ( showCards, player , hiddenCards);
-		replaceCard( showCards ,  player , hiddenCards );	
+		printCards(showCards , hiddenCards);
+		System.out.println("\nplay");
+                //Scanner throwCard = new Scanner(System.in);
+                //String played = throwCard.next();
+
+		//removeCard ( showCards, played );
+		//replaceCard( showCards ,  player , hiddenCards );
+		index( cardsInOrder , playerIIPlayCards, inTable[0] , inTable[5]);
+		System.out.println(Arrays.toString(playerIIPlayCards));
 		//printCards( showCards ,  hiddenCards);
-		//System.out.println(Arrays.toString(cards));
+		//System.out.println(a);
 		
 
 	
+		
+
 	
+	}
+
+ 
+
+	//finging the index of the bigger or lower card in playerII's cards
+	public static int indexBiggerLower (String[] playerIIPlayCards ,String[][] cardsInOrder, int indexInTable ){
+		int indexBiggerCard =-1;
+		
+		if(indexInTable == -1 ) 
+				indexInTable = 12;
+
+
+		if(indexInTable  == 13){
+			indexInTable =0;
+		}
+		if(indexInTable <13 && indexInTable > -1){
+			
+			for(int i=0 ; i<4 ; i++){
+				int index = Arrays.asList(playerIIPlayCards).indexOf(cardsInOrder[indexInTable][i]);
+				if(index !=-1){
+					indexBiggerCard = index;
+				}
+		
+			//System.out.println(index);
+			}
+		}
+		return indexBiggerCard;
+	}
+//when we have two cards bigger than table cards we play with cards that we have bigger or smaller of it
+	public static boolean twoIndexes( String[] playerIIPlayCards , String[][] cardsInOrder , int indexInTable){
+		 int lowerBigger=  indexBiggerLower(playerIIPlayCards , cardsInOrder ,indexInTable);
+		 if(lowerBigger != -1){
+			 return true ;
+		 }else 
+			 return false;
+	}
+                 
+
+
+
+//finding and replacing the bigger or lower card in table in playerII's cards
+	public static void findBiggerLower(String[] playerIIPlayCards ,String[][] cardsInOrder , int indexInTableI , int indexInTableII){
+		int indexBiggerI = -1;
+		int indexBiggerII=-1;
+		int indexLowerI =-1;
+		int indexLowerII= -1;
+		int checkI =0;
+		int check = 0 ;
+		if(indexInTableI != -1){
+			indexBiggerI = indexBiggerLower(playerIIPlayCards , cardsInOrder ,(indexInTableI+1) );
+			System.out.println("I  " + indexBiggerI);
+		}
+
+		if(indexInTableII != -1){
+                        indexBiggerII = indexBiggerLower(playerIIPlayCards , cardsInOrder , (indexInTableII+1));
+			System.out.println("II " +indexBiggerII);
+			
+		}
+		 //System.out.println(indexBiggerII);
+
+		if(indexBiggerI == -1 && indexBiggerII == -1 ){
+			indexLowerI = indexBiggerLower(playerIIPlayCards , cardsInOrder ,(indexInTableI-1));
+			indexLowerII = indexBiggerLower(playerIIPlayCards , cardsInOrder ,(indexInTableII-1));
+
+
+
+
+			if(indexLowerI != -1 & indexLowerII != -1){
+                        	boolean lowerI = twoIndexes ( playerIIPlayCards , cardsInOrder , indexInTableI-2);
+                        	boolean lowerII = twoIndexes ( playerIIPlayCards , cardsInOrder , indexInTableII -2);
+				boolean lowI = false;
+				boolean lowII = false;
+
+                        	if(lowerI == true || lowerII == true){
+                                	lowI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI));
+                                	lowII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII));
+				}
+
+
+
+				if(lowerI== true && lowI ==true){
+					playerIIPlayCards[indexLowerI]= null;
+					check =1;
+				}
+
+				else if(lowerII == true && lowII ==true){
+					playerIIPlayCards[indexLowerII] = null;
+					check =1;
+				}
+				else if (lowerI== true || lowI ==true){
+                                        playerIIPlayCards[indexLowerI]= null;
+					check =1;
+                                }
+				else if (lowerII == true || lowII ==true){
+					playerIIPlayCards[indexLowerII] = null;
+					check =1;
+                                }
+
+				if( lowerI ==false && lowerII == false ){
+					playerIIPlayCards[indexLowerI]= null;
+					check =1;
+				}
+			
+			}
+			if(check == 0 ){
+
+
+				if(indexLowerI != -1)
+					playerIIPlayCards[indexLowerI]=null;
+				else if(indexLowerII != -1)
+					playerIIPlayCards[indexLowerII] =null;
+			}
+			
+		}
+
+
+
+
+		if(indexBiggerI != -1 && indexBiggerII != -1){
+                        boolean biggerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI +2));
+                        boolean biggerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII +2));
+			boolean bigI =false;
+			boolean bigII =false;
+			if(biggerI == true || biggerII == true){
+				bigI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI ));
+                        	bigII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII ));
+			}
+
+			if(biggerI == true && bigI ==true){
+				playerIIPlayCards[indexBiggerI] = null ;
+				checkI = 1;
+			}
+			else if(biggerII == true && bigII ==true ) {
+				playerIIPlayCards[indexBiggerII] = null;
+				checkI = 1;
+			}
+			else if(biggerI == true || bigI ==true){
+                                playerIIPlayCards[indexBiggerI] = null ;
+                                checkI = 1;
+                        }
+                        else if(biggerII == true || bigII ==true ) {
+                                playerIIPlayCards[indexBiggerII] = null;
+                                checkI = 1;
+                        }
+
+			else if(biggerI == false && biggerII == false ){
+				 playerIIPlayCards[indexBiggerI] = null ;
+				 checkI =1;
+			}
+
+
+		}
+		if(checkI == 0){
+ 			if(indexBiggerI!= -1){
+				playerIIPlayCards[indexBiggerI] = null;
+                                	//System.out.println(indexBiggerI);
+                      	 }
+			else if (indexBiggerII != -1){
+				playerIIPlayCards[indexBiggerII]=null;
+			}
+		
+		}
+
+		if(indexBiggerI != -1 & indexLowerI != -1){
+		        boolean lowerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI -2));
+                        boolean biggerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI +2));
+			boolean low =false ;
+			boolean big =false ; 
+			//boolean lowerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII -2));
+                        //boolean biggerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII +2));
+
+                        if(biggerI != false || lowerI != false ){
+                                big = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI ));
+                        //        biggerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII ));
+				low = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI ));
+			//	lowerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII ));
+
+                        }
+
+                        if(biggerI == true &&  big == true)
+                                playerIIPlayCards[indexBiggerI] = null ;
+			else if(lowerI == true && low == true)
+					playerIIPlayCards[indexLowerI] =null;
+                        if (big == false & low == false ){
+                                 playerIIPlayCards[indexBiggerI] = null ;
+                                 checkI =1;
+
+                        }
+			if (biggerI == false & lowerI == false ){
+                                 playerIIPlayCards[indexBiggerI] = null ;
+                                 checkI =1;
+
+                        }
+
+
+                }
+		 if(indexBiggerI != -1 & indexLowerII != -1){
+                        //boolean lowerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI -2));
+                        boolean biggerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI +2));
+                        boolean lowerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII -2));
+                        //boolean biggerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII +2));
+
+                        if(biggerI == false && lowerII == false){
+                                biggerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI ));
+                                //biggerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII ));
+                                //lowerI = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableI ));
+                                lowerII = twoIndexes ( playerIIPlayCards , cardsInOrder , (indexInTableII ));
+
+                        }
+
+                        if(biggerI == true)
+                                playerIIPlayCards[indexBiggerI] = null ;
+                
+                        else if(lowerII == true)
+                                playerIIPlayCards[indexLowerII] = null;
+                        if (biggerI == false & lowerII == false){
+                                 playerIIPlayCards[indexBiggerI] = null ;
+                                 checkI =1;
+                        }
+
+
+                }
+
+
+
+                        //findBiggerCard( playerIIPlayCards , cardsInOrder , indexInTableI+1 , indexInTableII+1);
+		
+		//System.out.println(indexBiggerI);
+	
+		
 
 	}
+
+
+	
+
+
+//finding the index of the cards in the Table in cardsInOrder
+	public static void index( String[][] cardsInOrder , String[] playerIIPlayCards, String inTableI , String inTableII){
+		String[] hold = new String[4];
+		int indexInTableI = -1;
+                int indexInTableII = -1;
+		for(int i =0 ; i<13; i++){
+			for(int j=0 ; j<4 ; j++){
+				hold[j] = cardsInOrder[i][j];
+			}
+			indexInTableI = Arrays.asList(hold).indexOf(inTableI);
+			if(indexInTableI !=-1)
+				indexInTableI=i;
+			indexInTableII = Arrays.asList(hold).indexOf(inTableII);
+			if(indexInTableII !=-1)
+				indexInTableII =i;
+
+			findBiggerLower(playerIIPlayCards , cardsInOrder , indexInTableI , indexInTableII);
+			
+				//System.out.println(i);
+	//			System.out.println(indexInTableI);
+	//			System.out.println(indexInTableII);
+			
+	//			System.out.println(Arrays.toString(hold));
+		}
+	}
+		
+
+
+
+
+	public static String[][] ordersOfCards(){
+		String[][] cardsInOrder = { {"AK" ,"AD","AP","AG"},
+				            {"2K" ,"2D","2P","2G"},
+					    {"3K" ,"3D","3P","3G"},
+					    {"4K" ,"4D","4P","4G"},
+					    {"5K" ,"5D","5P","5G"} ,
+			  		    {"6K" ,"6D","6P","6G"},
+				            {"7K" ,"7D","7P","7G"},
+					    {"8K" ,"8D","8P","8G"},
+					    {"9K" ,"9D","9P","9G"},
+					    {"10K" ,"10D","10P","10G"},
+					    {"SoldierK","SoldierD","SoldierP","SoldierG"},
+					    {"QueenK", "QueenD","QueenP","QueenG"},		  
+					    {"KingK" ,"KingD","KingP","KingG"}};
+		return cardsInOrder;
+	}
+
+
+
+
+	public static String[] playerIIPlayCards(String[] playerII){
+		String[] playerIIPlayCards = new String[4];
+		for(int i=0 ; i<4 ; i++){
+			playerIIPlayCards[i]= playerII[i];
+		}
+		System.out.println(Arrays.toString(playerIIPlayCards));
+		return playerIIPlayCards;
+	}
+
+	
 	public static void tableCards(String[] inTable){
 		String[] playerITable = new String[5];
 		String[] playerIITable = new String[5];
@@ -31,12 +339,14 @@ class shuffleCards{
 			playerITable[i] = inTable[i];
 			playerIITable[i]= inTable[i+5];
 		}
+
 		printInTable(playerITable , playerIITable);
+		
 	}
 
 
 	public static void printInTable(String[] playerITable , String[] playerIITable){
-		System.out.println("in table : " + playerITable[0] + "  " +playerIITable[0]);
+		System.out.println("in table :    " + playerITable[0] + "     " +playerIITable[0]);
 	}
 
 
@@ -45,17 +355,14 @@ class shuffleCards{
 
 
 
-
-	public static void playCard (String[] showCards , String[] player , int[] hiddenCards){
-		System.out.println("\nplay");
-		Scanner throwCard = new Scanner(System.in);
-		String played = throwCard.next();
+	
+	public static void removeCard (String[] showCards , String played ){
+	//	System.out.println("\nplay");
+	//	Scanner throwCard = new Scanner(System.in);
+	//	String played = throwCard.next();
 		int indexRemoveCard = Arrays.asList(showCards).indexOf(played);
 		showCards[indexRemoveCard] = null;
 			
-		
-			//replaceCard( showCards , player , hiddenCards );
-		
 		
 	}
 
@@ -116,26 +423,37 @@ class shuffleCards{
 	public static String[] disturbiutCards (String[] shuffledCards){
 		String[] playerI = new String [21];
 		String[] playerII = new String [21];
-		String[] inTable = new String [10];
+		
 		for(int i=0 ; i<21 ; i++){
                 	playerI[i]=shuffledCards[i];
 			playerII[i]=shuffledCards[i+20];}
-		for(int j=0 ; j<10;j++){
-			inTable[j] = shuffledCards[j+42];
-		}
-		//showPlayerCards(playerI);
-		tableCards(inTable);
+		
+		
 		return playerI;
-
-
-
 		//System.out.println(Arrays.toString(playerI));
 		//System.out.println(Arrays.toString(playerII));
 		//System.out.println(Arrays.toString(inTable));
 		
 	}
 
+	public static String[] returnPlayerII(String[] shuffledCards){
+		String[] playerII = new String [21];
 
+                for(int i=0 ; i<21 ; i++){
+                         playerII[i]=shuffledCards[i+20];}
+		playerIIPlayCards(playerII);
+		return playerII;
+	}
+
+	public static String[] returInTable( String[] shuffledCards){
+		String[] inTable = new String [10];
+		for(int j=0 ; j<10;j++){
+                        inTable[j] = shuffledCards[j+42];
+                
+		}
+		tableCards(inTable);
+		return inTable;
+	}
 
 
 
